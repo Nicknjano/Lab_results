596616e96bea7552fff61757a4b70961313562db
Nicknjano <122860272+Nicknjano@users.noreply.github.com>
2023-04-18 11:52:48 +0300

    lab results repo

--- sample_survey/settings.py
+++ sample_survey/settings.py
@@ -76,9 +76,16 @@ WSGI_APPLICATION = 'sample_survey.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+     'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'Lab_feedback',
+        'USER': 'root',
+        'PASSWORD': 'nick254',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+        'OPTIONS': {  
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"  
+        } 
     }
 }
 
